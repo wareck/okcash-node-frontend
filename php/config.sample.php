c6aea871f6b800e2f37c01abdace78d04eba0553
wareck <wareck@gmail.com>
2021-07-03 08:02:07 +0200

    update config.sample.php with all options

--- php/config.sample.php
+++ php/config.sample.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Bitcoin Status Page
+ * Okcash Status Page
  *
  * @category File
- * @package  BitcoinStatus
+ * @package  OkcashStatus
  * @license  https://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
  */
 
@@ -16,9 +16,12 @@ $config = array(
   'rpc_ssl'                   => false,
   'rpc_ssl_ca'                => null,
 
-  // Donations
-  'display_donation_text'     => false,
-  'donation_address'          => 'not_set',
+  // Peers
+  'display_peer_info'         => false,
+  'display_peer_port'         => false,
+  'hide_dark_peers'           => true,
+  'ignore_unknown_ping'       => false,
+  'peers_to_ignore'           => array(),
 
   // Staking
   'display_stake_info'        => true,
@@ -27,11 +30,31 @@ $config = array(
   // Temperature
   'Raspi_temp'                => true,
 
+  // Cache
+  'cache_geo_data'            => false,
+  'geo_cache_file'            => '/var/tmp/okcashdd-geolocation.cache',
+  'geo_cache_time'            => 604800,
+  'use_cache'                 => true,
+  'cache_file'                => '/tmp/okcashd-status.cache',
+  'max_cache_time'            => 300,
+  'nocache_whitelist'         => array('127.0.0.1'),
+
+  // Geolocation
+  'geolocate_peer_ip'         => false,
+  'display_ip_location'       => false,
+
   // UI
   'display_free_disk_space'   => true,
   'display_version'           => true,
   'display_max_height'        => true,
   'use_bitcoind_ip'           => true,
+
+  // Node Count
+  'peercount_whitelist'       => array('127.0.0.1'),
+  'peercount_file'            => '/tmp/okcashd-peers.data',
+  'peercount_max_age'         => '2592000',
+  'peercount_min_data_points' => 10,
+  'peercount_extra_nodes'     => array(),
 
   // Uptime
   'bitcoind_process_name'     => 'okcashd',
@@ -41,6 +64,5 @@ $config = array(
   'disk_space_mount_point'    => 'DRV',
   'timezone'                  => null,
   'stylesheet'                => 'v2-light.css',
-  'debug'                     => false,
-  'admin_email'               => 'admin@mail.com',
+  'admin_email'               => 'admin@mymail.com',
 );
